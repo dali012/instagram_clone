d17237b6fbf8e88910f5befe4c35efd9b2f69a87
dali012 <dali.jerbi97@gmail.com>
2020-09-12 21:32:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <div className="app__headerImage">
+          <img alt="" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
